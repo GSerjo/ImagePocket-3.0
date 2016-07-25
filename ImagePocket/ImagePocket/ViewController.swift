37408aa7a0941e346bae8b66648eefa822b8dd42
GSerjo <sedot@gmail.com>
2016-07-24 23:46:34 -0400

    initial 

---
+++ ImagePocket/ImagePocket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImagePocket
+//
+//  Created by Serjo on 24/07/16.
+//  Copyright © 2016 Serjo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
