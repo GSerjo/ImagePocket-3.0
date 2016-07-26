9fab35343a55f96bbc2ce646fbf86cfa77d2ca92
GSerjo <sedot@gmail.com>
2016-07-26 02:12:42 -0400

    updated title

--- ImagePocket/ImagePocket/ViewController.swift
+++ ImagePocket/ImagePocket/ViewController.swift
@@ -10,32 +10,36 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    private let rootTitle = "Image Pocket"
+    
     private var openMenuButton: UIBarButtonItem!
+    
+    @IBOutlet weak var seletImageButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        configureToolbar()
+        self.title = rootTitle
         
-        // Do any additional setup after loading the view, typically from a nib.
+        configureToolbar()
     }
-
+    
+    @IBAction func onSelectClicked(sender: UIBarButtonItem) {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     
     private func configureToolbar(){
-
         openMenuButton = MMDrawerBarButtonItem(target: self, action: #selector(onOpenMenuClicked))
         navigationItem.leftBarButtonItem = openMenuButton
 
     }
     
     @objc private func onOpenMenuClicked(){
-        print("onOpenMenuClicked")
-        
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         appDelegate.centerContainer.toggleDrawerSide(MMDrawerSide.Left, animated: true, completion: nil)
     }
