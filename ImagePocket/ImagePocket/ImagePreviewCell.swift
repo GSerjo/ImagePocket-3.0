ccbc61c02585dc6be5fd058a45b72f7c1fdb9e0b
GSerjo <sedot8@gmail.com>
2016-08-08 00:39:42 -0400

    ImagePreviewCell added activity indicator

--- ImagePocket/ImagePocket/ImagePreviewCell.swift
+++ ImagePocket/ImagePocket/ImagePreviewCell.swift
@@ -10,9 +10,10 @@ import UIKit
 import Photos
 
 class ImagePreviewCell: UICollectionViewCell {
-
+    
     //let targetSize = CGSize(width: selectedAsset.pixelWidth, height: selectedAsset.pixelHeight)
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -21,16 +22,21 @@ class ImagePreviewCell: UICollectionViewCell {
     
     func setImage(asset: PHAsset){
         
-        /*let requestOptions = PHImageRequestOptions()
+        let requestOptions = PHImageRequestOptions()
         requestOptions.deliveryMode = PHImageRequestOptionsDeliveryMode.HighQualityFormat
         requestOptions.resizeMode = PHImageRequestOptionsResizeMode.Fast
         requestOptions.networkAccessAllowed = true
-        requestOptions.synchronous = false*/
+        requestOptions.synchronous = false
+        /*requestOptions.progressHandler = { [weak self]
+         (value: Double, _: NSError?, _ : UnsafeMutablePointer<ObjCBool>, _ : [NSObject : AnyObject]?) in
+         self!.activityIndicator.startAnimating()
+         }*/
         
         let targetSize = CGSize(width: asset.pixelWidth, height: asset.pixelHeight)
         
-        PHImageManager.defaultManager().requestImageForAsset(asset, targetSize: targetSize, contentMode: .AspectFill, options: nil){ [weak self](image, options) in
-                self!.imageView.image = image
+        PHImageManager.defaultManager().requestImageForAsset(asset, targetSize: targetSize, contentMode: .AspectFill, options: requestOptions){ [weak self](image, options) in
+            self!.imageView.image = image
+            self!.activityIndicator.stopAnimating()
         }
     }
 }
